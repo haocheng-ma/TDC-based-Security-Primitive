14f7232a0f3ff78cf74082d2c7f1e8d6dedb59a9
AndrewMzz <andrewmahc.gmail.com>
2020-01-15 16:38:15 +0800

    first commit

---
+++ TDC_IP_CORE/wrapper.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+module TDC(
+            input wire	clk_delay,
+			input wire	clk_sample,
+			output [63:0] data,
+			input wire	ena
+    );
+
+
+endmodule
